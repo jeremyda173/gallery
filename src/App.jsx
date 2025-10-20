5e2391c8bb85900b02fb4ddc0e4ecd869b613355
jeremyda173 <jeremygithud173@gmail.com>
2025-10-20 19:19:10 -0400

    feat: crear componente App principal

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Gallery from './components/Gallery'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <Gallery />
+    </div>
+  )
+}
+
+export default App
